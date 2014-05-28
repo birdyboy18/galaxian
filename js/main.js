961b904c6b1ff76c1be10702171440dbe5eed2a6
Paul Bird <paulbird1993@gmail.com>
2014-05-28 14:11:50 +0100

    implemented QuadTree Object for better collision detection

--- js/main.js
+++ js/main.js
@@ -79,6 +79,178 @@ function Drawable() {
 }
 
 /**
+	QuadTree Object
+	The Quadtree allows faster collision detection in the game by reducing the amount of
+	check the game nees to do based on what quadrant the object is in and any surrouding
+	objects that it could collide with.
+
+	The quadrant indexes are as followed:
+
+			|
+		1	|	0
+	--------|-------
+		2	|	3
+			|
+*/
+
+function QuadTree(boundBox,lvl) {
+	var maxObjects = 10;
+	this.bounds = boundBox || {
+		x:0,
+		y:0,
+		width: 0,
+		height:0
+	};
+	var objects = [];
+	this.nodes = [];
+	var level = lvl || 0;
+	var maxLevels = 5;
+	/**
+		Clears all the nodes of the QuadTree except for the objects, we want to keep those
+	*/
+	this.clear = function() {
+		objects = [];
+		for ( var i = 0; i < this.nodes.length; i++) {
+			this.nodes[i].clear();
+		}
+		this.nodes = [];
+	};
+	/**
+		This function will get all of the objects in the quadtree and return them
+		passing it back to the function so that you can use it
+	*/
+	this.getAllObjects = function(returnedObjects) {
+		for (var i = 0; i < this.nodes.length; i++) {
+			this.nodes[i].getAllObjects(returnedObjects);
+		}
+		for (var i = 0; len = objects.length; i < len; i++) {
+			returnedObjects.push(objects[i]);
+		}
+		return returnedObjects;
+	};
+	/**
+		This function returns all the objects that the given object might actually collide with
+	*/
+	this.findObjects = function(returnedObjects, obj) {
+		if (typeof obj === 'undefined') {
+			console.log("UNDEFINED OBJECT");
+			return;
+		}
+		var index = this.getIndex(obj);
+		if (index != -1 && this.nodes.length) {
+			this.nodes[index].findObjects(returnedObjects, obj);
+		}
+		for (var i = 0; len = objects.length; i < len; i++) {
+			returnedObjects.push(objects[i]);
+		}
+		return returnedObjects;
+	};
+	/**
+		This will insert an object into a QuadTree. If the tree is
+		bigger than the capacity then it will split it up and add all the objects to the right nodes in the tree
+	*/
+	this.insert = function(obj) {
+		if (typeof obj === 'undefined') {
+			return;
+		}
+		if (obj instanceof Array) {
+			for (var i = 0; len = obj.length; i < len; i++) {
+				this.insert(obj[i]);
+			}
+			return;
+		}
+		if (this.nodes.length) {
+			var index = this.getIndex(obj);
+			if (index != -1) {
+				this.nodes[index].insert(obj);
+				return;
+			}
+		}
+		objects.push(obj);
+		//Prevent infinite splitting
+		if (objects.length > maxObjects && level < maxLevels) {
+			if (this.nodes[0] = null) {
+				this.split();
+			}
+			var i = 0;
+			while (i < objects.length) {
+				var index = this.getIndex(objects[i]);
+				if (index != -1) {
+					this.nodes[index].insert((objects.splice(i,1))[0]);
+				} else {
+					i++;
+				}
+			}
+		}
+	};
+	/**
+		A function that determines which node the object belongs to, -1 means it doesn't fit
+		and is part of the current node
+	*/
+	this.getIndex = function(obj) {
+		var index = -1;
+		var verticalMidpoint = this.bounds.x + this.bounds.width/2;
+		var horizontalMidpoint = this.bounds.y + this.bounds.height/2;
+		// The Object can fit completely into the top quadrant
+		var topQuadrant = (obj.y < horizontalMidpoint && obj.y + obj.height < horizontalMidpoint);
+		// The object can fit completlty into the bottom quadrant
+		var bottomQuadrant = (obj.y > horizontalMidpoint);
+		// The object is capable of fitting completely within the left quadrants
+		if (obj.x < verticalMidpoint && obj.x + obj.width < verticalMidpoint) {
+			if (topQuadrant) {
+				index = 1;
+			}
+			else if (bottomQuadrant) {
+				index = 2;
+			}
+		}
+
+		//The Object can fit into the right quadrant
+		else if (obj.x > verticalMidpoint) {
+			if (topQuadrant) {
+				index = 0;
+			}
+			else if (bottomQuadrant) {
+				index = 3;
+			}
+		}
+		return index;
+	};
+	/**
+		This function splits a node into 4 more subnodes
+	*/
+	this.split = function() {
+		var subWidth = (this.bounds.width / 2) | 0;
+		var subHeight = (this.bounds.height / 2) | 0;
+
+		this.nodes[0] = new QuadTree({
+			x: this.bounds.x + subWidth,
+			y: this.bounds.y,
+			width: subWidth,
+			height: subHeight
+		}, level+1);
+		this.nodes[1] = new QuadTree({
+			x: this.bounds.x,
+			y: this.bounds.y,
+			width: subWidth,
+			height: subHeight
+		}, level+1);
+		this.nodes[2] = new QuadTree({
+			x: this.bounds.x,
+			y: this.bounds.y + subHeight,
+			width: subWidth,
+			height: subHeight
+		}, level+1);
+		this.nodes[3] = new QuadTree({
+			x: this.bounds.x + subWidth,
+			y: this.bounds.y + subHeight,
+			width: subWidth,
+			height: subHeight
+		}, level+1);
+	};
+}
+
+/**
 	Create a background object that is a child of drawable.
 	This will be drawn onto the background canvas and creates the illusion of a panning background
 	This is able to create multiple backgroun images, so it takes two parameters, the speed to move it at and which image we want to draw
