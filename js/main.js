fa4edda1a5184d8fdf6aa69c569e8ed31bb29dfc
Paul Bird <paulbird1993@gmail.com>
2014-05-28 14:19:45 +0100

    Added new abstract functions to Drawble class ready for collision detection

--- js/main.js
+++ js/main.js
@@ -72,9 +72,18 @@ function Drawable() {
 	this.speed = 0;
 	this.canvasWidth = 0;
 	this.canvasHeight = 0;
+	this.collidableWith = "";
+	this.isColliding = false;
+	this.type = "";
 
 	this.draw = function() {
 
+	};
+	this.move = function() {
+
+	};
+	this.isCollidableWith = function(obj) {
+		return (this.collidableWith === obj.type);
 	};
 }
 
